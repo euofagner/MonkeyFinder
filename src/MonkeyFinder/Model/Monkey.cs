982ec8e86c6534999d885800849c85f600ca046f
Fagner <fagnergeo0@gmail.com>
2024-12-04 17:45:24 -0300

    Setting the model 'monkey'

---
+++ src/MonkeyFinder/Model/Monkey.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyFinder.Model;
+
+class Monkey
+{
+    public class Root
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+        public string Image { get; set; }
+        public int Population { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
